8cc989a39fddbfa6758c2ca4ddaac591734f432e
Julien Chapron <jc271200001@protonmail.com>
2022-06-22 10:17:06 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+import {Box, Typography} from "@mui/material"
+function App() {
+  return (
+    <Box textAlign="center">
+    <Typography>Authentication with Keycloak</Typography>
+
+    </Box>
+  );
+}
+
+export default App;
